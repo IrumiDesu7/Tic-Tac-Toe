84e7399b505e5a68221ba1fa18a2cc8face97e8b
IrumiDesu7 <ikalam89@gmail.com>
2022-06-03 21:50:18 +0800

    added initial files

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const gameBoard = (() => {
-  let _board = new Array(9);
-
-  const add = (position, symbol) => {
-    _board = _board.splice(position, 0, symbol);
-    return _board;
-  };
-
-  return { _board, add };
+const gameBoard = (function () {
+  const _board = ['X', 'O', 'X', 'X', 'O', 'X', 'X', 'O', 'X'];
+  return { _board };
 })();
 
-const Player = (name, symbol) => {
-  return { name, symbol };
-};
+const displayController = (function () {
+  let boards = document.querySelectorAll('td');
+  boards = [...boards];
+  const boardUpdate = (() => {
+    return boards.map((item, index) => {
+      return (item.textContent = gameBoard._board[index]);
+    });
+  })();
+  return { boards };
+})();
